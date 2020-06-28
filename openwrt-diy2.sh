f01e36833135f778a94ba4390d8341417ce14d2f
Aunsetre <aunsetre@outlook.com>
2020-06-29 07:46:26 +0800

    Update openwrt-diy2.sh

--- openwrt-diy2.sh
+++ openwrt-diy2.sh
@@ -4,4 +4,4 @@ sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_genera
 wget https://github.com/upx/upx/releases/download/v3.96/upx-3.96-arm64_linux.tar.xz staging_dir/host/bin/
 xz -d upx-3.96-arm64_linux.tar.xz
 tar xf upx-3.96-arm64_linux.tar
-mv upx-3.96-arm64_linux upx
+mv upx-3.96-arm64_linux/upx ..
