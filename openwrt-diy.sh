131244c2e10c10e9915901efa9cd6101bf20d22c
Aunsetre <aunsetre@outlook.com>
2020-06-28 18:58:47 +0800

    Update openwrt-diy.sh

--- openwrt-diy.sh
+++ openwrt-diy.sh
@@ -45,7 +45,6 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git
 # passwall 依赖
 #brook  chinadns-ng   dns2socks  ipt2socks  kcptun  pdnsd-alt  simple-obfs  tcping  v2ray-plugin
 git clone https://github.com/Lienol/openwrt-package.git
-git clone https://github.com/kuoruan/openwrt-upx.git
 
 # 注意
 svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall/
