8cf04db3baa609b9c820a6f1d676452504e73536
Aunsetre <aunsetre@outlook.com>
2020-06-29 19:44:06 +0800

    Update openwrt-diy.sh

--- openwrt-diy.sh
+++ openwrt-diy.sh
@@ -39,27 +39,27 @@ git clone https://github.com/lisaac/luci-lib-docker.git
 #git clone https://github.com/kuoruan/luci-app-kcptun.git 
 
 # helloWorld
-git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/luci-app-vssr.git 
+#git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
+#git clone https://github.com/jerrykuku/luci-app-vssr.git 
 
 # passwall 依赖
 #brook  chinadns-ng   dns2socks  ipt2socks  kcptun  pdnsd-alt  simple-obfs  tcping  v2ray-plugin
-git clone https://github.com/Lienol/openwrt-package.git
+#git clone https://github.com/Lienol/openwrt-package.git
 
 # 注意
-svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall/
+#svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall/
  #
  #smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git
+#git clone https://github.com/pymumu/luci-app-smartdns.git
 #n1-install-program
 git clone https://github.com/tuanqing/install-program
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/parted
 
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-syncdial
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusic
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusicGo
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-unblockmusic
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-syncdial
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusic
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusicGo
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-unblockmusic
