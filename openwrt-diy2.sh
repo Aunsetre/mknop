0f492b617c0f22953878f018cb65f60faa759ff9
Aunsetre <aunsetre@outlook.com>
2020-06-29 15:59:32 +0800

    Update openwrt-diy2.sh

--- openwrt-diy2.sh
+++ openwrt-diy2.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
-svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
-echo 'tools-y += ucl upx'>>tools/Makefile
-echo '$(curdir)/upx/compile := $(curdir)/ucl/compile'>>tools/Makefile
 sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
 sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages.git' feeds.conf.default
 ./scripts/feeds update node
