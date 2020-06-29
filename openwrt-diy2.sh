13048a955ef774bdea37f0426e6a3d05d9089aa5
Aunsetre <aunsetre@outlook.com>
2020-06-29 08:15:34 +0800

    Update openwrt-diy2.sh

--- openwrt-diy2.sh
+++ openwrt-diy2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages.git' feeds.conf.default
+./scripts/feeds update node
+rm ./package/feeds/packages/node
+rm ./package/feeds/packages/node-*
+./scripts/feeds install -a -p node
 wget https://github.com/upx/upx/releases/download/v3.96/upx-3.96-arm64_linux.tar.xz
 xz -d upx-3.96-arm64_linux.tar.xz
 tar xf upx-3.96-arm64_linux.tar
