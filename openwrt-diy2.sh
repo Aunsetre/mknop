41e96b833b5e4b9018d26470a765224903f7b2f7
Aunsetre <aunsetre@outlook.com>
2020-06-30 11:45:32 +0800

    Update openwrt-diy2.sh

--- openwrt-diy2.sh
+++ openwrt-diy2.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
-sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages.git' feeds.conf.default
-./scripts/feeds update node
-rm ./package/feeds/packages/node
-rm ./package/feeds/packages/node-*
-./scripts/feeds install -a -p node
