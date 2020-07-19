8fee03be5366edd83086ea725390a05d3326bf5e
Aunsetre <aunsetre@gmail.com>
2020-07-20 03:44:43 +0800

    fix bug

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,30 +15,39 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
  
 cd package/lean/
-rm -rf luci-app-vssr  luci-lib-docker luci-app-dockerman luci-theme-argon luci-app-smartdns openwrt-smartdns luci-app-adguardhome openwrt-packages
+rm -rf luci-app-vssr adguardhome  luci-lib-docker luci-app-dockerman luci-theme-argon luci-app-smartdns openwrt-smartdns luci-app-adguardhome openwrt-packages
 rm -rf kcptun  lua-maxminddb small
+
 # adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/truck/adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+
 ### theme ###
 # luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 sed -i 's/Microsoft Yahei/Sarasa UI SC/ ' luci-theme-argon/htdocs/luci-static/argon/css/style.css
+
 #京东签到
 git clone https://github.com/jerrykuku/node-request.git
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+
 #docker
 git clone https://github.com/lisaac/luci-app-dockerman.git
 git clone https://github.com/lisaac/luci-lib-docker.git
+
 # helloWorld
 git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
 git clone https://github.com/jerrykuku/luci-app-vssr.git 
+
 # passwall 依赖
 #brook  chinadns-ng   dns2socks  ipt2socks  kcptun  pdnsd-alt  simple-obfs  tcping  v2ray-plugin
+
 git clone https://github.com/kenzok8/small.git
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall
+
 #smartdns
 git clone  https://github.com/pymumu/openwrt-smartdns.git
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
+
 #n1-install-program
 git clone https://github.com/tuanqing/install-program
